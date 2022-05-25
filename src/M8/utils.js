85b520d7b480895fdcd59821a85dd21064216771
andy <160149808@qq.com>
2022-05-25 09:10:34 +0800

    学习mapbox

--- src/M8/utils.js
+++ src/M8/utils.js
@@ -1,19 +1,19 @@
 export const colorRgb = function(color){
-  var sColor = color.toLowerCase();
+  let sColor = color.toLowerCase();
   //十六进制颜色值的正则表达式
-  var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+  let reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
   // 如果是16进制颜色
   if (sColor && reg.test(sColor)) {
       if (sColor.length === 4) {
-          var sColorNew = "#";
-          for (var i=1; i<4; i+=1) {
+          let sColorNew = "#";
+          for (let i=1; i<4; i+=1) {
               sColorNew += sColor.slice(i, i+1).concat(sColor.slice(i, i+1));    
           }
           sColor = sColorNew;
       }
       //处理六位的颜色值
-      var sColorChange = [];
-      for (var i=1; i<7; i+=2) {
+      let sColorChange = [];
+      for (let i=1; i<7; i+=2) {
           sColorChange.push(parseInt("0x"+sColor.slice(i, i+2)));    
       }
       return "rgb(" + sColorChange.join(",") + ")";
